9e4cfbc25060c969ffd29bd5380c969ca54615c2
Nicolle Gonzalez <nicolle@Nicolles-MBP.lan1>
2020-11-18 14:22:05 -0800

    Added Frontend and correctly whitelisted localhost:3000/

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware', 
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,6 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # we whitelist localhost:3000 because that's where frontend will be served
-# CORS_ORIGIN_WHITELIST = (
-#      'localhost:3000/'
-#  )
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
